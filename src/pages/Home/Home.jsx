49b55c740d7ba64071723f02eada580ece0a93f8
emonh0que <you@example.com>
2023-09-01 23:09:06 +0600

    added login page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import Categories from '../components/Categories/Categories';
+import Categories from '../../components/Categories/Categories';
+
 
 const Home = () => {
     return (
         <>
-          <Categories/>  
+         <Categories/>
         </>
     );
 };
