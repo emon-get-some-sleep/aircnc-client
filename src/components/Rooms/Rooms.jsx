501c89c8e7547641e46448c6616595f20572b11a
emonh0que <you@example.com>
2023-09-03 18:34:23 +0600

    added query string

--- src/components/Rooms/Rooms.jsx
+++ src/components/Rooms/Rooms.jsx
@@ -4,7 +4,11 @@ import { useState } from 'react';
 import Container from '../Shared/Container'
 import Card from './Card';
 import Loader from '../Shared/Loader/Loader';
-const Rooms = () => {
+import { useSearchParams } from 'react-router-dom';
+const Rooms = ({}) => {
+    const [params, setParams] = useSearchParams();
+    const category = params.get('category');
+    
     const [rooms, setRooms] = useState([]);
     const [loading, setLoading] = useState(false);
     useEffect(() => {
@@ -17,7 +21,8 @@ const Rooms = () => {
         })
         .catch(err => console.log(err))
     }, [])
-    console.log(rooms);
+    // console.log(rooms);
+   
 
     if (loading) {
         return <Loader/>
